021857d229b4c9782168d9cdb8a7673989e67975
tylerwise <twise@commandalkon.com>
2014-04-03 12:18:23 -0500

    changed controllers, partial1, and partial2

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,35 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope', function($scope) {
+		$scope.car = {make: 'Honda',
+					  model: 'Civic',
+					  year: 2014,
+					  price: 2400
+					  }
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MyCtrl2', ['$scope', function($scope) {
+  
+		$scope.cars = [
+						{make: 'Chevy',
+						 model: 'Tahoe',
+						 year: 2008,
+						 price: 12000
+						},
+						{make: 'Nissan',
+						 model: 'XTerra',
+						 year: 2008,
+						 price: 6000
+						},
+						{make: 'Honda',
+						 model: 'Accord',
+						 year: 2013,
+						 price: 18000
+						},
+						{make: 'Toyota',
+						 model: 'Tacoma',
+						 year: 2001,
+						 price: 2000
+						}
+					 ];	 
   }]);
